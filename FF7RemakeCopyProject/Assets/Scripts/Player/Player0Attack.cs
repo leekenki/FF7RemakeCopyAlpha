5c1d26111b4db558cb87dddeaa8300d965f09197
leekenki <leekenki88@gmail.com>
2022-08-12 17:35:54 +0900

    2022 08 12 1735
    Commit

--- FF7RemakeCopyProject/Assets/Scripts/Player/Player0Attack.cs
+++ FF7RemakeCopyProject/Assets/Scripts/Player/Player0Attack.cs
@@ -20,23 +20,12 @@ public class Player0Attack : PlayerAttackBase
     #endregion
 
 
+    #region
+    // PlayerBase 약공격, 강공격(310 번줄에) 아래에 짜서 만들어 놓으면 됩니다.
+    // 키 눌렀을 때 바로 들어갈 수 있도록
+    #endregion
     private void Update()
     {       
-        // 약 공격 버튼 (단축키)
-        if(Input.GetKeyDown(KeyCode.Keypad1))
-        {
-
-        }
-        // 어빌리티 공격 버튼 (단축키)
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-
-        }
-        // 마테리아 스킬 버튼 (단축키)
-        if (Input.GetKeyDown(KeyCode.Keypad3))
-        {
-
-        }
 
     }
 
@@ -44,10 +33,10 @@ public class Player0Attack : PlayerAttackBase
     public override void WeekAttack(GameObject target)
     {
         #region 영재 주석 ( 강공격 )
-        //버튼을 누를 때 시간을 재기 시작하여 0.5초이상 누르고 1초내에 떼어 다시 0.5초 내에 버튼을 누르면 강공격을 한다.
+        // 버튼을 누를 때 시간을 재기 시작하여 0.5초이상 누르고 1초내에 떼어 다시 0.5초 내에 버튼을 누르면 강공격을 한다.
         // 0.5초 미만에서 떼면 0초~0.5초 사이에 몇번을 누르던 약공격한번,
-        //1초 넘게 누르고 있으면 최초 약 공격 한번만
-        //0.5초 ~ 1초 사이동안 누르기 성공 후 0.5초 내에 버튼을 누르면 강공격 0.5초 이후에 버튼을 누르면 약공격
+        // 1초 넘게 누르고 있으면 최초 약 공격 한번만
+        // 0.5초 ~ 1초 사이동안 누르기 성공 후 0.5초 내에 버튼을 누르면 강공격 0.5초 이후에 버튼을 누르면 약공격
         // if(Physics.Raycast(transform.position, transform.forward, out hitInfo))
         // {
         //     Debug.Log("임시 weekattack"); 
@@ -57,11 +46,10 @@ public class Player0Attack : PlayerAttackBase
         //         DamageManager.Instance.CalculateDamage(gameObject, randomDamage, target.GetComponent<EnemyHealth>());
         //     }
         // }
-        //int randomDamage = UnityEngine.Random.Range(100, 200);
-        //DamageManager.Instance.CalculateDamage(gameObject, randomDamage, target.GetComponent<EnemyHealth>());
+        // int randomDamage = UnityEngine.Random.Range(100, 200);
+        // DamageManager.Instance.CalculateDamage(gameObject, randomDamage, target.GetComponent<EnemyHealth>());
         #endregion
 
-       
         Debug.Log("WeekAttack");
     }
 
